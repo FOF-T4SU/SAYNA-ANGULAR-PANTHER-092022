39adc843e6c3c663ac2b1635f724aec43c33ec62
FOF-T4SU <fof.t4su.sayna@gmail.com>
2022-09-21 23:37:56 +0000

    creation des composants

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WakandaComponent } from './components/wakanda/wakanda.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MonCompteComponent } from './components/mon-compte/mon-compte.component';
+import { ProduitComponent } from './components/produit/produit.component';
+import { PanierComponent } from './components/panier/panier.component';
+import { InscriptionComponent } from './components/inscription/inscription.component';
+import { ConnexionComponent } from './components/connexion/connexion.component';
+import { InfoProduitComponent } from './components/info-produit/info-produit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WakandaComponent,
+    FooterComponent,
+    MonCompteComponent,
+    ProduitComponent,
+    PanierComponent,
+    InscriptionComponent,
+    ConnexionComponent,
+    InfoProduitComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/info-produit/info-produit.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-info-produit',
+  templateUrl: './info-produit.component.html',
+  styleUrls: ['./info-produit.component.scss']
+})
+export class InfoProduitComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
